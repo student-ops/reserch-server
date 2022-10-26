ec8b00b1f748d1d069c6ba633fc12946ba40696f
student-ops <gogakuec@gmail.com>
2022-10-26 15:31:39 +0900

    add speaker

---
+++ speaker-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8081"
+
+type Config struct{}
+
+func main(){
+	app := Config{}
+
+	log.Printf("Starting speaker service on port %s\n",webPort)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s",webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil{
+		log.Panic(err)
+	}
+}
